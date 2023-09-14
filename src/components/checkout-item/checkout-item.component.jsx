f993c7180befb3bfba6f2d7a74e95d210b6647a2
Thomas Nelson <tcnelson13@gmail.com>
2023-09-14 13:51:23 -0500

    branching prior to redux implementation

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,41 @@
+import { CartContext } from '../../contexts/cart.context';
+import { useContext } from 'react';
+
+import {
+  Arrow,
+  BaseSpan,
+  CheckOutItemContainer,
+  ImageContainer,
+  Quantity,
+  RemoveButton,
+  Value,
+} from './checkout-item.styles';
+
+const CheckoutItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+
+  const { clearItemFromCart, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
+
+  const clearItemHandler = () => clearItemFromCart(cartItem);
+  const addItemHandler = () => addItemToCart(cartItem);
+  const removeItemHandler = () => removeItemFromCart(cartItem);
+
+  return (
+    <CheckOutItemContainer>
+      <ImageContainer>
+        <img src={imageUrl} alt={`${name}`} />
+      </ImageContainer>
+      <BaseSpan>{name}</BaseSpan>
+      <Quantity>
+        <Arrow onClick={removeItemHandler}>&#10094;</Arrow>
+        <Value>{quantity}</Value>
+        <Arrow onClick={addItemHandler}>&#10095;</Arrow>
+      </Quantity>
+      <BaseSpan>{price}</BaseSpan>
+      <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
+    </CheckOutItemContainer>
+  );
+};
+
+export default CheckoutItem;
